ac31c34393b7669de31778eeaf660476ef148c9b
Cal <calvin.m71@hotmail.com>
2025-01-09 13:29:25 -0800

    update

--- Tutoring/graph/map.py
+++ Tutoring/graph/map.py
@@ -1,4 +1,5 @@
 from graph import connect, Node
+from queue import Queue
 
 # read file, 0 = not connected, 1 = connected
 # line one = names of nodes
@@ -20,6 +21,39 @@ def reader(fileName: str) -> dict[str, Node[str]]:
                     connect(graph[cities[i]], graph[cities[j]])
             
     return graph
+
+# functions (graph, starting city, ending city)
+# returns all paths, shortest to longest
+
+def travel(graph: dict[str, Node[str]], city_A: str, city_B: str):
+    # shortest path, loop through cities, count amount in array, return smallest value
+    # BFS explores all neighbors before progressing to deeper level
+    # DFS goes as deep as possible before back tracking
+    # BFS = Shortest path
+    # DFS = Longest path
+
+    paths_seen = set()
+    queue = Queue()
+    queue.put([city_A])
+    def shortest_path(path, city):
+        length = 0
+        for city, connections in graph:
+            pass
+    
+    def dfs(node, city, visited):
+        if node in visited:
+            return None
+        if node.data == city:
+            return node
+        copy_v = set(visited)
+        copy_v.add(node)
+        for child in node.connections:
+            result = dfs(child, city, copy_v)
+            if result is not None:
+                return result
+        return None
+
+
 
 fileDir = "/Users/calvinmalagon/Documents/GitHub/Masters_Program/Tutoring/graph/map.dat"
 
